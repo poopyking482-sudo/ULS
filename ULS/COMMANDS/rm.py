c91368fbecdcee70ab6e3fdb5552e69dddb5c8b3
Xetch <poopyking482@gmail.com>
2026-06-28 15:40:12 +0800

    Create ULS/COMMANDS/rm.py

---
+++ ULS/COMMANDS/rm.py
@@ -0,0 +1,12 @@
+import shutil
+
+def run(args):
+    if len(args) < 2:
+        print("error: please specify which subdirectory")
+        return
+    target_dir = args[1]
+    try:
+        shutil.rmtree(target_dir)
+        print(f"[uls] successfully deleted: {target_dir}")
+    except Exception as e:
+        print(f"error: {e}")
