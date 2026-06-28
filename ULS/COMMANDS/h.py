947c67cdacef0864e50c1c17f4424f41340f3cfa
Xetch <poopyking482@gmail.com>
2026-06-28 15:42:17 +0800

    Update h.py

--- ULS/COMMANDS/h.py
+++ ULS/COMMANDS/h.py
@@ -2,6 +2,6 @@ import os
 
 def run():
     home_dir = os.path.expanduser("~")
-    print(f"--- Home Folder Contents ({home_dir}) ---")
+    print(f"--- home folder contents ({home_dir}) ---")
     for entry in os.listdir(home_dir):
         print(entry)
