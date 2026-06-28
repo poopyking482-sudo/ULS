38570654a76b149942a847b4a70418de47dbf696
Xetch <poopyking482@gmail.com>
2026-06-28 15:44:43 +0800

    Create CONSOLE/uls.py

---
+++ ULS/CONSOLE/uls.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+# link together by pointing
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from commands import flags
+
+def main():
+    args = sys.argv[1:]
+    flags.handle_args(args)
+
+if __name__ == "__main__":
+    main()
