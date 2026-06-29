7aa03c1b76b27623d88f82b0ceff30823f6c24a2
Xetch <poopyking482@gmail.com>
2026-06-29 20:45:38 +0800

    Update blk.py

--- ULS/COMMANDS/blk.py
+++ ULS/COMMANDS/blk.py
@@ -1,4 +1,21 @@
 import subprocess
-
+import platform
 def run():
+if platform.system == 'Linux':
     subprocess.run(["lsblk"])
+# check if MacOS
+elif platform.system == 'Darwin':
+subprocess.run(["diskutil", "list"])
+# check if run WINDOWS
+elif platform.system == 'Windows':
+subprocess.run(["wmic", "list", "diskdrive", "brief"], shell=True)
+elif platform.system == 'NetBSD':
+subprocess.run(["drvctl", -l"])
+# psst, slide me a push request in blk.py
+elif platform.system == 'FreeBSD':
+subprocess.run(["sysctl", hw.disknames"])
+# why? I wanna support all Unix systems
+elif platform.sysyem == 'OpenBSD':
+subprocess.run(["geom", "disk", "list""])
+# don't say to support more
+# I just like to show this disk drive
